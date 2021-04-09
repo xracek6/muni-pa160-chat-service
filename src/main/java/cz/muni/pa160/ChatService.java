90fb5596adf848db8eb8ce91790074d7c1955784
Jakub Racek <43374750+xracek6@users.noreply.github.com>
2021-04-09 10:12:18 +0200

    Update ChatService.java

--- src/main/java/cz/muni/pa160/ChatService.java
+++ src/main/java/cz/muni/pa160/ChatService.java
@@ -1,6 +1,6 @@
 package cz.muni.pa160;
 
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
